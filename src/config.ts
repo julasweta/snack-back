4ba41f2380a1ecd866530d6fff23f1ecf453db9a
julasweta <julasweta@ukr.net>
2024-07-06 02:58:30 +0300

    pg

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 import pg from "pg";
 
-dotenv.config();
 
 const postgresUrl = "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
 
