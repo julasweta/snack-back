777494536d8ddd00ba06981582f07c962c0975cb
julasweta <julasweta@ukr.net>
2024-07-06 12:53:09 +0300

    pg

--- dist/config.js
+++ dist/config.js
@@ -5,15 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
-const pg_1 = __importDefault(require("pg"));
 dotenv_1.default.config();
 const postgresUrl = "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
-if (!postgresUrl) {
+if (!process.env.POSTGRES_DATABASE) {
     throw new Error("POSTGRES_URL environment variable is not defined");
 }
-const sequelize = new sequelize_1.Sequelize(postgresUrl, {
-    dialect: "postgres",
-    dialectModule: pg_1.default,
+const sequelize = new sequelize_1.Sequelize(process.env.POSTGRES_DATABASE, {
     logging: false,
 });
 exports.default = sequelize;
--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const postgresUrl = "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
 
-if (!postgresUrl) {
+if (!process.env.POSTGRES_DATABASE) {
   throw new Error("POSTGRES_URL environment variable is not defined");
-}
-
-const sequelize = new Sequelize(postgresUrl);
+} 
+const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, {
+  logging: false,
+});
 
 export default sequelize;
