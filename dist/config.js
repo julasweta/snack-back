f680c1a775a5327cf8b223810010c3b74e557f2b
julasweta <julasweta@ukr.net>
2024-07-06 13:28:58 +0300

    pg

--- dist/config.js
+++ dist/config.js
@@ -6,11 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const postgresUrl = "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
-if (!process.env.POSTGRES_DATABASE) {
+const postgresUrl = process.env.POSTGRES_URL;
+if (!postgresUrl) {
     throw new Error("POSTGRES_URL environment variable is not defined");
 }
-const sequelize = new sequelize_1.Sequelize(process.env.POSTGRES_DATABASE, {
+const sequelize = new sequelize_1.Sequelize(postgresUrl ||
+    "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
+    dialect: "postgres",
     logging: false,
 });
 exports.default = sequelize;
