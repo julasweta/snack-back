2225b99a378f22aab07af477d4206213f5ed421b
julasweta <julasweta@ukr.net>
2024-07-06 13:00:06 +0300

    pg

--- src/config.ts
+++ src/config.ts
@@ -3,12 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const postgresUrl = "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
+const postgresUrl = process.env.POSTGRES_URL;
 
-if (!process.env.POSTGRES_DATABASE) {
+if (!postgresUrl) {
   throw new Error("POSTGRES_URL environment variable is not defined");
-} 
-const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, {
+}
+
+const sequelize = new Sequelize(postgresUrl, {
   logging: false,
 });
 
