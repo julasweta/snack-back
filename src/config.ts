d5578944d8a83d3a4cbbba0cf4f11d1c97305b5b
julasweta <julasweta@ukr.net>
2024-07-06 19:13:47 +0300

    change;env

--- src/config.ts
+++ src/config.ts
@@ -3,16 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config(); // Завантажує змінні оточення з .env файлу
 
-
-const sequelize = new Sequelize(
-  `postgresql://postgress:GgrrROvO1tuJjxgc73PoJsclaJY6n2Mf@dpg-cq4i5mo8fa8c73fprar0-a.virginia-postgres.render.com/snack_9fzy`,
-  {
-    dialectOptions: {
-      ssl: {
-        require: true, // Вимагати захищене з'єднання
-        rejectUnauthorized: false, // Відхиляти неавторизовані SSL сертифікати (необхідно в деяких середовищах)
-      },
+const postgresUrl = process.env.POSTGRES_URL;
+const sequelize = new Sequelize(postgresUrl? postgresUrl: '', {
+  dialectOptions: {
+    ssl: {
+      require: true, // Вимагати захищене з'єднання
+      rejectUnauthorized: false, // Відхиляти неавторизовані SSL сертифікати (необхідно в деяких середовищах)
     },
-  }
-);
+  },
+});
 export default sequelize;
