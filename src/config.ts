299a36722d8475f311fc1bbb8c80098874415c54
julasweta <julasweta@ukr.net>
2024-07-06 13:27:06 +0300

    pg

--- src/config.ts
+++ src/config.ts
@@ -9,9 +9,13 @@ if (!postgresUrl) {
   throw new Error("POSTGRES_URL environment variable is not defined");
 }
 
-const sequelize = new Sequelize(postgresUrl, {
-  dialect: "postgres",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  postgresUrl ||
+    "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  {
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 export default sequelize;
