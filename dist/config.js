abb0c86995b94f2f32d40a4c735329798bbf92f1
julasweta <julasweta@ukr.net>
2024-07-06 13:43:02 +0300

    pg

--- dist/config.js
+++ dist/config.js
@@ -6,13 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const postgresUrl = process.env.POSTGRES_URL;
-if (!postgresUrl) {
-    throw new Error("POSTGRES_URL environment variable is not defined");
-}
-const sequelize = new sequelize_1.Sequelize(postgresUrl ||
-    "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
+//const postgresUrl = process.env.POSTGRES_URL;
+//console.log("POSTGRES_URL:", postgresUrl);
+/* if (!postgresUrl) {
+  throw new Error("POSTGRES_URL environment variable is not defined");
+} */
+//console.log("POSTGRES_URL: after", postgresUrl);
+const sequelize = new sequelize_1.Sequelize("postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
     dialect: "postgres",
-    logging: false,
+    logging: (sql, timing) => {
+        console.log("SQL Query:", sql); // Виводимо SQL запит
+        console.log("Execution time:", timing, "ms"); // Виводимо час виконання запиту
+        console.log("Environment Variables:", process.env); // Виводимо всі змінні середовища
+    },
 });
 exports.default = sequelize;
--- src/config.ts
+++ src/config.ts
@@ -3,16 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const postgresUrl = process.env.POSTGRES_URL;
-console.log("POSTGRES_URL:", postgresUrl);
+//const postgresUrl = process.env.POSTGRES_URL;
+//console.log("POSTGRES_URL:", postgresUrl);
 
-if (!postgresUrl) {
+/* if (!postgresUrl) {
   throw new Error("POSTGRES_URL environment variable is not defined");
-}
-console.log("POSTGRES_URL: after", postgresUrl);
+} */
+//console.log("POSTGRES_URL: after", postgresUrl);
 
 const sequelize = new Sequelize(
-  postgresUrl ||
     "postgres://default:pkMLPqsy1R5u@ep-frosty-dawn-a4g5za0q-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
   {
     dialect: "postgres",
